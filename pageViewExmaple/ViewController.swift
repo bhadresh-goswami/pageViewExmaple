64fceb6bf662d4f7031a7c1cc39f0c2f3e16649c
Sanket Chauhan <sanketchauhan.cmpica@gmail.com>
2019-02-22 12:00:31 +0530

    Initial Commit

---
+++ pageViewExmaple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pageViewExmaple
+//
+//  Created by Mac on 22/02/19.
+//  Copyright © 2019 bhadresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
